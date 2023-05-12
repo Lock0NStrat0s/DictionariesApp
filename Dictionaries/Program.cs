d7579404a3ad03f146f5e787c46fc55bd49ef3aa
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-11 23:27:43 -0600

    code complete.

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -1,2 +1,25 @@
 ﻿
 
+Dictionary<string, string> lookup = new Dictionary<string, string>();
+
+lookup["animal"] = "Not a human";
+lookup["fish"] = "Not a human that swims";
+lookup["human"] = "Us";
+
+Console.WriteLine($"The definition of fish is {lookup["fish"]}");
+
+Dictionary<int, string> employees = new Dictionary<int, string>();
+
+employees[95] = "Tim Corey";
+employees[28] = "Mohammad Jabir";
+
+Console.WriteLine($"The employee with ID number 28 is {employees[28]}");
+
+Dictionary<string, int> dayOfWeek = new Dictionary<string, int>();
+
+dayOfWeek["Wednesday"] = 4;
+dayOfWeek["Thursday"] = 5;
+dayOfWeek["Friday"] = 6;
+
+Console.WriteLine($"Wednesday is day number {dayOfWeek["Wednesday"]}");
+
